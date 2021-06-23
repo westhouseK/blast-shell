11ef33ef0c530db3cf35a2cfb0abe3e276191a17
westhouseK <nishiiekazuki@gmail.com>
2021-06-23 23:28:46 +0900

    can multiselect db

--- exec.sh
+++ exec.sh
@@ -9,7 +9,7 @@
 # ------------------------------------------------
 
 # 定数を宣言
-# ファイル郡
+# ディレクトリ名
 readonly DB_DIR="db"
 readonly QUERY_DIR="query"
 readonly OUTPUT_DIR="output"
@@ -17,17 +17,26 @@ readonly RESULT_DIR="result"
 readonly SEQUENCE_DIR="sequence"
 readonly LOG_FILE="error.log"
 
+# 対話式の選択肢
+readonly ENTER="Enter"
+readonly ALL="All"
+readonly NEXT="Next"
+readonly EXIT="Exit"
+
 # blastのオプション
 readonly OUTPUT_FORMAT="7 sseqid evalue score bitscore length qseqid pident qseq sseq"
 readonly NUM_ALIGN_OPTION="-num_alignments"
 readonly NUM_ALIGN_OPTION_NUM="1"
 
-# TODO: ログファイルを出力する
+# 出力ファイルのフォーマット
+readonly RESULT_FILE_FORMAT="%s_output%s.txt"
+readonly SEQUENCE_FILE_FORMAT="%s_sequence%s.fasta"
+readonly ABSTRUCTED_FASTA_FILE_FORMAT="%s.fasta"
 
-# 実行の確認を行う-------------------------------------------------------------------------
-echo "------------------------------------"
+# 事前チェック-------------------------------------------------------------------------
+echo "-----------------------------------------"
 echo "blastnを実行しますか？ (y or Enter/n)"
-echo "------------------------------------"
+echo "-----------------------------------------"
 read input
 
 if [ ! $input = 'y' ]; then
@@ -37,66 +46,62 @@ fi
 
 # ディレクトリが存在しない場合、エラー
 if [ ! -d $DB_DIR -o ! -d $QUERY_DIR -o ! -d $OUTPUT_DIR -o ! -d $OUTPUT_DIR/$RESULT_DIR -o ! -d $OUTPUT_DIR/$SEQUENCE_DIR ]; then
-    echo "ディレクトリ作成がまだです。"
+    echo "ディレクトリの作成を先に行ってください。"
     echo "setup.shを実行してください。"
     exit 1
 fi
 
-# データベースが存在しない場合、エラー
-if [ -z "$(ls ./$DB_DIR)" ]; then 
-    echo "データベースが存在しません。"
-    exit 1
-fi
-
-# クエリが存在しない場合、エラー
-if [ -z "$(ls ./$QUERY_DIR)" ]; then 
-    echo "クエリが存在しません。"
-    exit 1
-fi
-
-# TODO: データベースを複数選択できるようにする
-echo "------------------------------------"
-echo "データベースを選択してください。"
-echo "------------------------------------"
-select DB_NAME in $(ls ./$DB_DIR) "exit"
+# 選択-------------------------------------------------------------------------
+echo "-----------------------------------------"
+echo "クエリを選択してください。"
+echo " [$EXIT] -> 終了"
+echo "-----------------------------------------"
+select query_name in $(ls ./$QUERY_DIR) $EXIT 
 do
-    if [ $DB_NAME = "exit" ]; then
+    if [ $query_name = $EXIT ]; then
         echo "スクリプトを終了しました。"
         exit 1
     fi
     break
 done
 
-echo "------------------------------------"
-echo "クエリを選択してください。複数選択が可能です。"
-echo "Enterを押すと、再び選択肢が表示されます。"
-echo "次に進みたい場合は、「that's it」を選択してください。"
-echo "------------------------------------"
-query_names=()
-select QUERY_NAME in $(ls ./$QUERY_DIR) "that's it" "exit"
+echo "-----------------------------------------"
+echo "データベースを選択してください。"
+echo " [$ENTER] -> 選択肢を表示"
+echo " [$ALL]   -> すべてのDBに対してクエリを実行"
+echo " [$NEXT]  -> 次に進む"
+echo " [$EXIT]  -> 終了"
+echo "-----------------------------------------"
+db_names=()
+select db_name in $ALL $(ls ./$DB_DIR) $NEXT $EXIT 
 do
-    case $QUERY_NAME in 
-        "exit")
+    case $db_name in 
+        $EXIT)
             echo "スクリプトを終了しました。"
             exit 1;;
-        "that's it")
-            if [ ${#query_names[@]} -eq 0 ]; then
+        $ALL)
+            db_names=$(ls ./$DB_DIR)
+            echo "すべてを選択しました。"
+            echo "選択完了です。"
+            break;;
+        $NEXT)
+            if [ ${#db_names[@]} -eq 0 ]; then
                 echo "クエリを選択していないため、終了しました。"
                 exit 1
             fi
-            echo "選択完了しました。"
+            echo "選択完了です。"
             break;;
         *)
-            echo "続いて選択してください。"
-            query_names+=($QUERY_NAME);;
+            db_names+=($db_name)
+            echo "続いて選択してください。";;
     esac
 done
 
-echo "------------------------------------"
-echo "こちらで実行してもよろしいですか？ (y or Enter/n)"
-echo "選択したデータベース: $DB_NAME"
-echo "選択したクエリ: $(echo ${query_names[@]} | sed -e 's/ /, /g')"
-echo "------------------------------------"
+echo "-----------------------------------------"
+echo "以下の条件で実行してもよろしいですか？ (y or Enter/n)"
+echo "選択したクエリ: $query_name"
+echo "選択したデータベース: $(echo ${db_names[@]} | sed -e 's/ /, /g')"
+echo "-----------------------------------------"
 read input
 
 if [ ! $input = 'y' ]; then
@@ -104,21 +109,22 @@ if [ ! $input = 'y' ]; then
     exit 1
 fi
 
-# blast-------------------------------------------------------------------------
+# メインの処理(blastn)-------------------------------------------------------------------------
 # 出力先のファイルをフォーマット
-# ex. 2021-1-31_12:34 (実行するPCのタイムゾーンに依存。確認方法: cat /etc/sysconfig/clock)
-DATE=$(date '+%Y-%m-%d_%H:%M')
-# ファイル名を作成するためにMAX値を取得する
-FILE_COUNT=$(find ./$OUTPUT_DIR/$RESULT_DIR -type f -not -name '.*' | wc -l | tr -d ' ')
+# NOTE: 2021-1-31_12:34 (実行するPCのタイムゾーンに依存。確認方法: cat /etc/sysconfig/clock)
+date=$(date '+%Y-%m-%d_%H:%M')
+# 結果出力ファイル名のシーケンスを行うために、MAX値+1を取得
+file_sequence_num=$(find ./$OUTPUT_DIR/$RESULT_DIR -type f -not -name '.*' | wc -l | tr -d ' ' | xargs -Imax expr max + 1)
+sequence=file_sequence_num
 
-COUNT=1
-for query_name in ${query_names[@]}
+for db_name in ${db_names[@]}
 do
-    # blastの実行-------------------------------------------------------------------------
-    OUTPUT_NAME=$DATE'_output'$(expr $FILE_COUNT + $COUNT)'.txt'
-    ABSTRUCT_SEQUENCE=$DATE'_sequence'$(expr $FILE_COUNT + $COUNT)'.fasta'
-    echo "blastnを実行しています。"
-    blastn -db $DB_DIR/$DB_NAME/$DB_NAME -query $QUERY_DIR/$query_name -out $OUTPUT_DIR/$RESULT_DIR/$OUTPUT_NAME -outfmt "$OUTPUT_FORMAT" $NUM_ALIGN_OPTION "$NUM_ALIGN_OPTION_NUM" 2>> $LOG_FILE
+    # blastnの実行-------------------------------------------------------------------------
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    result_file_name="$(printf $RESULT_FILE_FORMAT $date $sequence)"
+    sequence_file_name="$(printf $SEQUENCE_FILE_FORMAT $date $file_sequence_num)" # 一番若い番号に集約
+    echo "blantnを実行中[クエリ($query_name) -> データベース($db_name)]"
+    blastn -db $DB_DIR/$db_name/$db_name.fasta -query $QUERY_DIR/$query_name -out $OUTPUT_DIR/$RESULT_DIR/$result_file_name -outfmt "$OUTPUT_FORMAT" $NUM_ALIGN_OPTION "$NUM_ALIGN_OPTION_NUM" 2>> $LOG_FILE
 
     # blastが失敗した時、エラー
     if [ $? -gt 0 ]; then
@@ -127,12 +133,10 @@ do
     fi
 
     echo "blastnの実行に成功しました！"
-    echo "続いて、塩基配列の抽出を行います。"
-
 
     # 先頭に「#」がついていない行を抽出し、配列に整形したのち重複を削除する
     # NOTE: BSD grepだと「-P」オプションがないため動かない。「grep -ve '#'」にする必要がある
-    unique_subject_ids=$(grep -P '^(?!#)' ./$OUTPUT_DIR/$RESULT_DIR/$OUTPUT_NAME | cut -f1 | awk '!a[$0]++' | tr -s '\n' ' ')
+    unique_subject_ids=$(grep -P '^(?!#)' ./$OUTPUT_DIR/$RESULT_DIR/$result_file_name | cut -f1 | awk '!a[$0]++' | tr -s '\n' ' ')
 
     # 空か判定する
     if [ -z $unique_subject_ids ]; then
@@ -140,15 +144,17 @@ do
         continue
     fi
 
+    echo "続いて、$(printf $ABSTRUCTED_FASTA_FILE_FORMAT $db_name)から抽出を行います。"
+
     # ファイルの抽出-------------------------------------------------------------------------
-    # fastaを読み込む
     for subject_id in ${unique_subject_ids[@]}
     do
-        cat ./db/$DB_NAME/$DB_NAME | grep $subject_id -A 1 >> ./$OUTPUT_DIR/$SEQUENCE_DIR/$ABSTRUCT_SEQUENCE
+        cat ./db/$db_name/"$(printf $ABSTRUCTED_FASTA_FILE_FORMAT $db_name)" | grep $subject_id -A 1 >> ./$OUTPUT_DIR/$SEQUENCE_DIR/$sequence_file_name
     done
 
-    echo "subject idと塩基配列の抽出に成功しました。"
-    let COUNT++ # ループするためにインクリメントする
+    echo "$(echo ${unique_subject_ids[@]} | sed -e 's/ /, /g')の${#unique_subject_ids[@]}件がヒットしました。"
+    expr sequence + 1 # ループするためにインクリメントする
 done
 
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "すべての処理を終了しました。"   
